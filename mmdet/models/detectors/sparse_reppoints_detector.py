499c764bac7dec4c513b238842fabdd1f4834d0a
ChunhuaLuo <luochunhua1996@outlook.com>
2021-02-07 10:46:24 +0000

    添加了detector

--- mmdet/models/detectors/sparse_reppoints_detector.py
+++ mmdet/models/detectors/sparse_reppoints_detector.py
@@ -1,6 +1,8 @@
 from ..builder import DETECTORS
 from .single_stage import SingleStageDetector
-
+import torch
+import torch.nn as nn 
+from mmdet.core import bbox2result
 
 @DETECTORS.register_module()
 class SparseRepPointsDetector(SingleStageDetector):
@@ -15,3 +17,32 @@ class SparseRepPointsDetector(SingleStageDetector):
                              train_cfg, 
                              test_cfg, 
                              pretrained)
+    
+    def simple_test(self, img, img_metas, rescale=False):
+        """Test function without test time augmentation.
+
+        Args:
+            imgs (list[torch.Tensor]): List of multiple images
+            img_metas (list[dict]): List of image information.
+            rescale (bool, optional): Whether to rescale the results.
+                Defaults to False.
+
+        Returns:
+            list[list[np.ndarray]]: BBox results of each image and classes.
+                The outer list corresponds to each image. The inner list
+                corresponds to each class.
+        """
+        x = self.extract_feat(img)
+        objectness, topn_box, topn_cls, topn_idx = self.bbox_head(x)
+        # bbox_pred, cls_pred, img_metas, cfg=None, rescale=False, with_nms=False
+        bbox_list = self.bbox_head.get_bboxes(
+            topn_box, topn_cls, img_metas, rescale=rescale)
+        # skip post-processing when exporting to ONNX
+        if torch.onnx.is_in_onnx_export():
+            return bbox_list
+
+        bbox_results = [
+            bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
+            for det_bboxes, det_labels in bbox_list
+        ]
+        return bbox_results
