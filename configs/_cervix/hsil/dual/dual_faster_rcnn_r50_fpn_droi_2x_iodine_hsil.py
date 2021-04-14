d24514adfa42c58882cc25c00fbdf87e6661583f
luochunhua <luochunhua1996@outlook.com>
2021-04-14 13:12:25 +0000

    fix bug in dual configs

--- configs/_cervix/hsil/dual/dual_faster_rcnn_r50_fpn_droi_2x_iodine_hsil.py
+++ configs/_cervix/hsil/dual/dual_faster_rcnn_r50_fpn_droi_2x_iodine_hsil.py
@@ -1,6 +1,6 @@
 _base_ = [
-    '../../_base_/schedules/schedule_2x.py',
-    '../../_base_/default_runtime.py',
+    '../../../_base_/schedules/schedule_2x.py',
+    '../../../_base_/default_runtime.py',
     './dual_base_iodine.py'
 ]
 model = dict(
