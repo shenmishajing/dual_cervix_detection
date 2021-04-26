15a3a632189a6512b5c92200f25fd3e2e24e3383
luochunhua <luochunhua1996@outlook.com>
2021-04-26 20:05:51 +0800

    add support for fpnfusecat

---
+++ configs/_cervix/hsil/dual/dual_dualdet_faster_rcnn_r50_fpn_droi_fpnfusecat_2x_acid_hsil.py
@@ -0,0 +1,12 @@
+_base_ = [
+    './dual_dualdet_faster_rcnn_r50_fpn_droi_fpnfuse_2x_acid_hsil.py'
+]
+
+model = dict(
+    roi_head=dict(
+        fpn_fuser_cfg=dict(
+            in_channels=512, #! prim + aux = 256 + 256
+            out_channels=256,
+            fuse_type="cat"
+        ),
+     ))
