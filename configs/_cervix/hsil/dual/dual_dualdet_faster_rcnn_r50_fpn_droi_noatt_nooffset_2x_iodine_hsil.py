47eaef650da2fc2b4a3d052f20ee01c747bda369
luochunhua <luochunhua1996@outlook.com>
2021-04-26 20:08:07 +0800

    add configs for no attention, no offset

---
+++ configs/_cervix/hsil/dual/dual_dualdet_faster_rcnn_r50_fpn_droi_noatt_nooffset_2x_iodine_hsil.py
@@ -0,0 +1,14 @@
+_base_ = [
+    './dual_dualdet_faster_rcnn_r50_fpn_droi_2x_iodine_hsil.py'
+]
+
+
+model = dict(
+    roi_head=dict(
+        attention_cfg=None,
+        offset_cfg=None,
+        bridge_bbox_droi_extractor=None,
+        prim_bbox_head=dict(
+            in_channels=256 
+            )
+        ))
