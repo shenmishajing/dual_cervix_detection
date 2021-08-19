5b4728585316d74677c63c6d34d5fcffe18d90ff
shenmishajing <shenmishajing@gmail.com>
2021-08-19 10:54:59 +0800

    add faster rcnn late fusion no shared model config files

---
+++ configs/faster_rcnn_late_fusion/faster_rcnn_late_fusion_no_shared_model_r50_fpn_2x_iodine.py
@@ -0,0 +1,21 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_late_fusion_r50_fpn.py',
+    '../_base_/datasets/dual_cervix/hsil/dual/dual_base.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+
+# model settings
+model = dict(
+    prim = 'iodine',
+    no_shared_modules = ['backbone', 'neck', 'rpn_head', 'roi_head'],
+    roi_head = dict(bbox_head = dict(num_classes = 1)))
+
+log_config = dict(
+    interval = 50,
+    hooks = [
+        dict(type = 'TextLoggerHook'),
+        dict(type = 'WandbLoggerHook',
+             with_step = False,
+             init_kwargs = dict(project = 'dual_cervix_detection',
+                                name = 'faster_rcnn_late_fusion_no_shared_model_r50_fpn_2x_iodine',
+                                tags = ['mmdetection', 'faster_rcnn_late_fusion_no_shared_model', 'r50', 'fpn', '2x', 'iodine']))])
