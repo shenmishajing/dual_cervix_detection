b1510ffad88b15b8c18f628bd360cb5872152d9c
shenmishajing <shenmishajing@gmail.com>
2021-08-24 10:24:46 +0800

    middle fusion channel attention reuse fusion module init code in middle fusion

--- mmdet/models/detectors/faster_rcnn_middle_fusion_channel_attention.py
+++ mmdet/models/detectors/faster_rcnn_middle_fusion_channel_attention.py
@@ -10,13 +10,13 @@ class FasterRCNNMiddleFusionChannelAttention(FasterRCNNMiddleFusion):
     """Implementation of `Faster R-CNN <https://arxiv.org/abs/1506.01497>`_"""
 
     def __init__(self, pool_size = 1, *args, **kwargs):
-        super(FasterRCNNMiddleFusion, self).__init__(*args, **kwargs)
+        super(FasterRCNNMiddleFusionChannelAttention, self).__init__(*args, **kwargs)
 
         pool_area = pool_size ** 2
         middle_channels = 256
         self.max_pool = nn.AdaptiveMaxPool2d((pool_size, pool_size))
         self.avg_pool = nn.AdaptiveAvgPool2d((pool_size, pool_size))
-        self.fusion_module = nn.ModuleList([
+        self.attn_module = nn.ModuleList([
             nn.ModuleList([
                 nn.Linear(self.neck[self.stages[0]].out_channels * 2 * pool_area, middle_channels),
                 nn.ReLU(),
@@ -27,22 +27,18 @@ class FasterRCNNMiddleFusionChannelAttention(FasterRCNNMiddleFusion):
             ])
             for _ in range(self.neck[self.stages[0]].num_outs)
         ])
-        self.down_channel_module = nn.ModuleList([
-            nn.Conv2d(self.neck[self.stages[0]].out_channels * 2, self.neck[self.stages[0]].out_channels, 1)
-            for _ in range(self.neck[self.stages[0]].num_outs)
-        ])
 
     def feature_fusion(self, acid_feats, iodine_feats):
         feats = []
-        for i in range(len(self.fusion_module)):
+        for i in range(len(self.attn_module)):
             acid_feat = acid_feats[i]
             iodine_feat = iodine_feats[i]
             feat = torch.cat([acid_feat, iodine_feat], dim = 1)
             attn = self.max_pool(feat) + self.avg_pool(feat)
             attn = attn.reshape(attn.shape[0], -1)
-            for m in self.fusion_module[i]:
+            for m in self.attn_module[i]:
                 attn = m(attn)
             feat = feat * attn[..., None, None]
-            feat = self.down_channel_module[i](feat)
+            feat = self.fusion_module[i](feat)
             feats.append(feat)
         return feats
