a5e00a2268c5eb50dc60933259ebf204832ef4f5
huheping <huheping@zju.edu.cn>
2021-09-14 11:18:43 +0800

    add config

---
+++ configs/cervix_project/iodine/faster_rcnn_r50_fpn_2x_iodine_hsil.py
@@ -0,0 +1,32 @@
+_base_ = [
+    '../../_base_/models/faster_rcnn_r50_fpn.py',
+    '../../_base_/schedules/schedule_2x.py',
+    '../../_base_/default_runtime.py',
+    './iodine_base.py'
+]
+
+model = dict(roi_head=dict(bbox_head=dict(num_classes=2)))
+
+optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
+
+
+classes = ("hsil",)
+
+
+data = dict(
+    train=dict(
+        classes=classes),
+    val=dict(
+        classes=classes
+        ),
+    test=dict(
+        classes=classes
+        ))
+
+
+
+
+
+
+
+
