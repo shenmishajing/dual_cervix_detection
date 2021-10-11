486c04eaf5749dc453b195e66d435923188bdb0f
huheping <huheping@zju.edu.cn>
2021-10-11 14:22:42 +0800

    add iodine hsil config

---
+++ configs/cervix_project/iodine_hsil/atss_r50_fpn_1x_iodine_hsil_totalanno.py
@@ -0,0 +1,29 @@
+_base_ = [
+    '../../_base_/models/atss_r50_fpn.py',
+    '../../_base_/schedules/schedule_1x.py',
+    '../../_base_/default_runtime.py',
+    './iodine_hsil_base.py'
+]
+
+model = dict(bbox_head=dict(num_classes=1))
+
+optimizer = dict(type='SGD', lr=0.00125, momentum=0.9, weight_decay=0.0001)
+
+data_root = 'data/cervix_project/detection/'
+data = dict(
+    train=dict(
+        ann_file=data_root + 'annos_hsil_frompaper/train_iodine.json'),
+    val=dict(
+        ann_file=data_root + 'annos_hsil_frompaper/val_iodine.json'
+        ),
+    test=dict(
+        ann_file=data_root + 'annos_hsil_frompaper/test_iodine.json'
+        ))
+
+
+
+
+
+
+
+
